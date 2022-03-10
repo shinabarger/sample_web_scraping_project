af866028f1782f0e0edffbcdb362436b41e76d05
dshinabarger <davidshinabarger@gmail.com>
2022-03-10 16:10:21 -0500

    Removed some redundant driver launches

--- web_scraping_project1.py
+++ web_scraping_project1.py
@@ -15,25 +15,20 @@ s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 
 
-def launchBrowser():
-    driver.get('https://eprel.ec.europa.eu/screen/product/tyres/657426')
-    return driver
+#def launchBrowser():
+#    driver.get('https://eprel.ec.europa.eu/screen/product/tyres/657426')
+#    return driver
 
+i = 657426
 
-i = 657425
-
-while i < 657426:
+while i <= 657426:
     request_string = ("https://eprel.ec.europa.eu/screen/product/tyres/" + str(i))
     driver.get(request_string)
     page = requests.get(request_string)
-
     html_source = driver.page_source
-
-    driver.get(request_string)
-    driver = launchBrowser()
-    driver.find_element(By.XPATH, "//*").get_attribute("outerHTML")
+#    driver = launchBrowser()
     print(request_string + " status is " + str(page.status_code))
-    #    print(driver.page_source)
+#    print(driver.page_source)
 
     commercial_name = driver.find_elements(By.XPATH, "//*[@id='ecl-main-content']/div/app-detail-page/ux-block-content/div/app"
                                                      "-detail/div/div/div[1]/ecl-accordion/ecl-accordion-item["
@@ -81,6 +76,28 @@ while i < 657426:
                                           "2]/app-parameter-item/app-parameter-item-template/div/div[2]/div/span[1]")
     wet_grip_classes = [x.text for x in wet_grip_class]
     print("7. Wet grip class: " + str(wet_grip_classes))
+
+    external_rolling_noise_class = driver.find_elements(By.XPATH, "//*[@id='ecl-main-content']/div/app-detail-page/ux-block-content/div/app-detail/div/div/div["
+                                                                  "1]/ecl-accordion/ecl-accordion-item[1]/div/div/app-tyre-parameters/app-detail-parameter-template["
+                                                                  "4]/div/div[2]/app-parameters-combine-item/app-parameter-item-template/div/div[2]/div/span[1]")
+    external_rolling_noise_classes = [x.text for x in external_rolling_noise_class]
+    print("8. Rolling Noise Class: " + str(external_rolling_noise_classes))
+
+    external_rolling_noise_level = driver.find_elements(By.XPATH, "//*[@id='ecl-main-content']/div/app-detail-page/ux-block-content/div/app-detail/div/div/div[1]/ecl-accordion/ecl-accordion-item[1]/div/div/app-tyre-parameters/app-detail-parameter-template[4]/div/div[2]/app-parameters-combine-item/app-parameter-item-template/div/div[2]/div/span[4]")
+    external_rolling_noise_levels = [x.text for x in external_rolling_noise_level]
+    print("9. Rolling Noise Level: " + str(external_rolling_noise_levels))
+
+    tyre_snow_condition = driver.find_elements(By.XPATH, "//*[@id='ecl-main-content']/div/app-detail-page/ux-block-content/div/app-detail/div/div/div[1]/ecl-accordion/ecl-accordion-item[1]/div/div/app-tyre-parameters/app-detail-parameter-template[5]/div/div[2]/app-parameter-item/app-parameter-item-template/div/div[2]/div/span")
+    tyre_snow_conditions = [x.text for x in tyre_snow_condition]
+    print("10. Tyre for use in severe snow conditions: ", str(tyre_snow_conditions))
+
+    tyre_ice_condition = driver.find_elements(By.XPATH,
+                                               "//*[@id='ecl-main-content']/div/app-detail-page/ux-block-content/div/app-detail/div/div/div[1]/ecl-accordion/ecl-accordion-item[1]/div/div/app-tyre-parameters/app-detail-parameter-template[6]/div/div[2]/app-parameter-item/app-parameter-item-template/div/div[2]/div/span")
+    tyre_ice_conditions = [x.text for x in tyre_ice_condition]
+    print("11. Tyre for use in severe ice conditions: ", str(tyre_ice_conditions))
+
+
+
 
     driver.close()
 
